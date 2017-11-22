3758b0890f7bab2f64035f4dfa34abe7932897a3
Love Joseph <ljoseph@mail.uoguelph.ca>
2017-11-22 00:02:44 -0500

    more work

--- app/src/main/java/com/example/lovejoseph/caremmunity/MainActivity.java
+++ app/src/main/java/com/example/lovejoseph/caremmunity/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.lovejoseph.caremmunity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+//import android.app.FragmentManager;
+
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,11 +17,24 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
+
+
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ViewPager mViewPager;
+    private MyPagerAdapter mAdapter;
+
+
+    // The fragments that are used as the individual pages
+    private final Fragment[] PAGES = new Fragment[] {
+            new Homefrag()
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,18 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mViewPager = (ViewPager) findViewById(R.id.mainPager);
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        mAdapter.addFragment(new Homefrag());
+        mAdapter.addFragment(new Homefrag());
+        mAdapter.addFragment(new Homefrag());
+        mAdapter.addFragment(new Homefrag());
+        mViewPager.setAdapter(mAdapter);
+
+
+        TabLayout tabber = (TabLayout) findViewById(R.id.tabberLayout);
+        tabber.setupWithViewPager(mViewPager);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -32,6 +64,43 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /* PagerAdapter for supplying the ViewPager with the pages (fragments) to display. */
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private SparseArray<Fragment> mFragments;
+        public MyPagerAdapter(FragmentManager manager) {
+            super(manager);
+            this.mFragments = new SparseArray<>();
+        }
+
+        public void addFragment(Fragment f) {
+            this.mFragments.append(mFragments.size() , f);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return this.mFragments == null ? null : this.mFragments.get(position) ;
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return this.mFragments.indexOfValue((Fragment) object);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            super.destroyItem(container, position, object);
+            this.mFragments.remove(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+
     }
 
     @Override
